beacc1494a5b8ec1346aa4245a0648b874dc495d
Mazen Mohamed <ma3en.mohamed@gmail.com>
2025-02-27 21:27:31 +0200

    fix bugs

--- src/app/collections/[collectionType]/layout.js
+++ src/app/collections/[collectionType]/layout.js
@@ -1,22 +1,5 @@
-import CollectionHeaderSkeleton from "@/_components/CollectionHeaderSkeleton";
-import FilterSectionSkeleton from "@/_components/FilterSectionSkeleton";
-import ProductsSkeleton from "@/_components/ProductsSkeleton";
-import { Suspense } from "react";
-
 function layout({ children }) {
-  return (
-    <Suspense
-      fallback={
-        <>
-          <CollectionHeaderSkeleton />
-          <FilterSectionSkeleton />
-          <ProductsSkeleton />
-        </>
-      }
-    >
-      {children}
-    </Suspense>
-  );
+  return <>{children};</>;
 }
 
 export default layout;
--- src/app/collections/layout.js
+++ src/app/collections/layout.js
@@ -1,26 +1,5 @@
-import CollectionHeaderSkeleton from "@/_components/CollectionHeaderSkeleton";
-import { Suspense } from "react";
-import Skeleton from "react-loading-skeleton";
-
 function layout({ children }) {
-  return (
-    <Suspense
-      fallback={
-        <>
-          <CollectionHeaderSkeleton />
-          <div className="max-w-[1200px] m-auto grid grid-cols-2 sm:grid-cols-3 gap-2 sm:gap-10 py-10 px-5">
-            {Array.from({ length: 6 }, (_, i) => i + 1).map((i) => (
-              <div className="relative aspect-[2.5/3] h-full" key={i}>
-                <Skeleton height="100%" width="100%" className="!rounded-lg" />
-              </div>
-            ))}
-          </div>
-        </>
-      }
-    >
-      {children}
-    </Suspense>
-  );
+  return <>{children};</>;
 }
 
 export default layout;
